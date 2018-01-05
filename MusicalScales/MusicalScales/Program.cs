c8cc6ef229bb93b8d66507662ccea6416ed70a5f
alexgrove <alexander_grove@hotmail.com>
2018-01-05 13:03:05 -0700

    built the basic program

--- MusicalScales/MusicalScales/Program.cs
+++ MusicalScales/MusicalScales/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MusicalScales
 {
@@ -6,8 +7,29 @@ namespace MusicalScales
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] scales = { "a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"};
+            Console.WriteLine("Please enter your major scale: ");
+            int userScale = Array.IndexOf(scales, Console.ReadLine());
+
+
+            string[] solfegeFinder = { scales[userScale], scales[userScale += 2], scales[userScale += 2], scales[userScale += 1], scales[userScale += 2], scales[userScale += 2], scales[userScale += 2] };
+
+
+            Console.Write("The notes in your scale are: ");
+            foreach (string note in solfegeFinder)
+            {
+                Console.Write(note + ", ");
+            }
+            
+            Console.Write("\n\nPlease enter the solfege name of your note: ");
+
+            string[] solfege = { "do", "re", "mi", "fa", "so", "la", "ti" };
+
+            int solfegeIndex = Array.IndexOf(solfege, Console.ReadLine());
+
+            Console.WriteLine("The note you are looking for is: " + solfegeFinder[solfegeIndex]);
             Console.ReadKey();
+
         }
     }
 }
